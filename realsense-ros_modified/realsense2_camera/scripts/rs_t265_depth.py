a871afd41efb4b1ffb1c8f59236a4dce92784322
rafaelfgs <rafaelfernandesgsilva@hotmail.com>
2020-02-27 23:20:28 -0300

    Adding rs_d435 modifications

--- realsense-ros_modified/realsense2_camera/scripts/rs_t265_depth.py
+++ realsense-ros_modified/realsense2_camera/scripts/rs_t265_depth.py
@@ -2,10 +2,9 @@
 
 import rospy
 import cv2
-#import sys
 import time
+import sys
 import numpy as np
-#import pyrealsense2 as rs
 from copy import copy
 from math import tan, pi
 from threading import Lock
@@ -13,8 +12,9 @@ from cv_bridge import CvBridge
 from sensor_msgs.msg import Image, CameraInfo
 import std_msgs.msg as std
 
-image_size = 100
-freq_rate = 10
+image_size = 400
+freq_rate = 0.2
+mode = "none" #"stack""overlay"
 
 R = np.array([[ 0.9999756813050,  0.004391118884090,  0.005417240317910],
               [-0.0043891328387,  0.999990284443000, -0.000378685304895],
@@ -35,7 +35,11 @@ def callback_img2(data):
     img2_data = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
 
 def callback_info1(data):
-    global K_left, D_left
+    global K_left, D_left, time_diff
+    if data.header.stamp.to_sec() < 1581534822:
+        time_diff = rospy.Time.now().to_sec() - data.header.stamp.to_sec()
+    else:
+        time_diff = 0.0
     K_left = np.reshape(data.K,(3,3))
     D_left = np.array(data.D[0:4])
 
@@ -44,23 +48,30 @@ def callback_info2(data):
     K_right = np.reshape(data.K,(3,3))
     D_right = np.array(data.D[0:4])
 
-def rs_depth():
+def realsense_depth():
     
-    rospy.init_node("rs_depth", anonymous=True)
+    global mode, frame_data, center_undistorted, disparity, disp_corrected, disp_vis, disp_color, color_image
     
-    rospy.Subscriber("/camera/fisheye1/image_raw", Image, callback_img1)
-    rospy.Subscriber("/camera/fisheye2/image_raw", Image, callback_img2)
-    rospy.Subscriber("/camera/fisheye1/camera_info", CameraInfo, callback_info1)
-    rospy.Subscriber("/camera/fisheye2/camera_info", CameraInfo, callback_info2)
+    rospy.init_node("realsense_depth", anonymous=True)
     
-    pub_depth_image = rospy.Publisher("/camera/stereo/depth/image", Image, queue_size=1)
-    pub_color_image = rospy.Publisher("/camera/stereo/color/image", Image, queue_size=1)
-    pub_rgb_image = rospy.Publisher("/camera/stereo/rgb/image", Image, queue_size=1)
-    pub_depth_info = rospy.Publisher("/camera/stereo/depth/camera_info", CameraInfo, queue_size=1)
-    pub_color_info = rospy.Publisher("/camera/stereo/color/camera_info", CameraInfo, queue_size=1)
-    pub_rgb_info = rospy.Publisher("/camera/stereo/rgb/camera_info", CameraInfo, queue_size=1)
+    rospy.Subscriber("/realsense/fisheye1/image_raw", Image, callback_img1)
+    rospy.Subscriber("/realsense/fisheye2/image_raw", Image, callback_img2)
+    rospy.Subscriber("/realsense/fisheye1/camera_info", CameraInfo, callback_info1)
+    rospy.Subscriber("/realsense/fisheye2/camera_info", CameraInfo, callback_info2)
     
-    time.sleep(1)
+    pub_depth_info  = rospy.Publisher("/realsense/stereo/depth/camera_info", CameraInfo, queue_size=1)
+    pub_depth_image = rospy.Publisher("/realsense/stereo/depth/image_raw", Image, queue_size=1)
+    pub_color_info  = rospy.Publisher("/realsense/stereo/color/camera_info", CameraInfo, queue_size=1)
+    pub_color_image = rospy.Publisher("/realsense/stereo/color/image_raw", Image, queue_size=1)
+    pub_rgb_info    = rospy.Publisher("/realsense/stereo/rgb/camera_info", CameraInfo, queue_size=1)
+    pub_rgb_image   = rospy.Publisher("/realsense/stereo/rgb/image_raw", Image, queue_size=1)
+    
+    sys.stdout.write('\nWaiting for "/realsense" publications... ')
+    sys.stdout.flush()
+    while not 'time_diff' in globals():
+        time.sleep(0.1)
+    sys.stdout.write('Ok!\n\n')
+    sys.stdout.flush()
     
     window_size = 5
     min_disp = 0
@@ -102,30 +113,30 @@ def rs_depth():
     msg_info.height = image_size
     msg_info.width = image_size
     msg_info.distortion_model = "plumb_bob"
-    msg_info.K = K_left.flatten()
-    msg_info.D = D_left.flatten()
-    msg_info.R = R_left.flatten()
-    msg_info.P = P_left.flatten()
+    msg_info.K = [0.8*image_size, 0, 0.5*image_size, 0, 0.8*image_size, 0.5*image_size, 0, 0, 1]#K_left.flatten()
+    msg_info.D = [0, 0, 0, 0, 0]#D_left.flatten()
+    msg_info.R = [1, 0, 0, 0, 1, 0, 0, 0, 1]#R_left.flatten()
+    msg_info.P = [0.8*image_size, 0, 0.5*image_size, 0, 0, 0.8*image_size, 0.5*image_size,0 , 0, 0, 1, 0]#P_left.flatten()
     
-    WINDOW_TITLE = "Realsense"
-    cv2.namedWindow(WINDOW_TITLE, cv2.WINDOW_NORMAL)
-    mode = "stack"
+    if not mode == "none":
+        cv2.namedWindow("realsense", cv2.WINDOW_NORMAL)
+
+    rate = rospy.Rate(freq_rate)
     count = 0
     
-    rate = rospy.Rate(freq_rate)
-    
-    while not rospy.is_shutdown():
+    while not rospy.is_shutdown() and time_diff > 0:
         
+        #t=time.time()
         count += 1
         
         frame_mutex.acquire()
         frame_data = {"left"  : copy(img1_data),
                       "right" : copy(img2_data),
                       "seq"   : copy(count),
-                      "stamp" : rospy.Time.now(),
-                      "frame" : "/camera_fisheye1_optical_frame"}
+                      "stamp" : rospy.Time.from_sec(rospy.Time.to_sec(rospy.Time.now()) - time_diff),
+                      "frame" : "realsense_depth_optical_frame"}
         frame_mutex.release()
-            
+        
         center_undistorted = {"left" : cv2.remap(src = frame_data["left"],
                                       map1 = undistort_rectify["left"][0],
                                       map2 = undistort_rectify["left"][1],
@@ -134,13 +145,14 @@ def rs_depth():
                                       map1 = undistort_rectify["right"][0],
                                       map2 = undistort_rectify["right"][1],
                                       interpolation = cv2.INTER_LINEAR)}
-                                      
+        
         disparity = stereo.compute(center_undistorted["left"], center_undistorted["right"]).astype(np.float32)
         disparity[disparity<0.0] = 0.0
         disparity_cropped = disparity[:,max_disp:] / 16.0
         
-        disp_corrected = 5000.0/(disparity[:,max_disp:]**1.5+500.0)+0.1
-        disp_corrected[disp_corrected==10.1] = 0.0
+#        disp_corrected = 5000.0/(disparity[:,max_disp:]**1.5+500.0)+0.1
+        disp_corrected = 850/(disparity[:,max_disp:]+80.0)-0.4
+        disp_corrected[disp_corrected>6.0] = 0.0
         
         disp_vis = 255*(disparity_cropped - min_disp) / num_disp
         disp_color = cv2.applyColorMap(cv2.convertScaleAbs(disp_vis,1), cv2.COLORMAP_JET)
@@ -148,7 +160,7 @@ def rs_depth():
         
         msg_depth = bridge.cv2_to_imgmsg(disp_corrected, encoding="passthrough")
         msg_color = bridge.cv2_to_imgmsg(disp_color, encoding="passthrough")
-        msg_rgb = bridge.cv2_to_imgmsg(color_image, encoding="passthrough")
+        msg_rgb = bridge.cv2_to_imgmsg(color_image, encoding="rgb8")
         
         msg_header.seq = frame_data["seq"]
         msg_header.stamp = frame_data["stamp"]
@@ -166,42 +178,28 @@ def rs_depth():
         pub_color_info.publish(msg_info)
         pub_rgb_info.publish(msg_info)
         
-#        if mode == "stack":
-#            cv2.imshow(WINDOW_TITLE, np.hstack((color_image, disp_color)))
-#        if mode == "overlay":
-#            ind = disparity_cropped >= min_disp
-#            color_image[ind, 0] = disp_color[ind, 0]
-#            color_image[ind, 1] = disp_color[ind, 1]
-#            color_image[ind, 2] = disp_color[ind, 2]
-#            cv2.imshow(WINDOW_TITLE, color_image)
-#        key = cv2.waitKey(1)
-#        if key == ord('s'): mode = "stack"
-#        if key == ord('o'): mode = "overlay"
-#        if key == ord('q') or cv2.getWindowProperty(WINDOW_TITLE, cv2.WND_PROP_VISIBLE) < 1:
-#            break
-
-        if not mode == "":
+        if not mode == "none":
             
             if mode == "stack":
                 img_window = np.hstack((color_image, disp_color))
-                cv2.imshow(WINDOW_TITLE, img_window)
+                cv2.imshow("realsense", img_window)
                 
             if mode == "overlay":
                 img_window = disp_color
                 img_window[disparity_cropped<min_disp] = 0.0
-                cv2.imshow(WINDOW_TITLE, img_window)
+                cv2.imshow("realsense", img_window)
                 
             key = cv2.waitKey(1)
             
             if key == ord("s"): mode = "stack"
-            if key == ord('o'): mode = "overlay"
-            if cv2.getWindowProperty(WINDOW_TITLE, cv2.WND_PROP_VISIBLE) < 1: mode = ""
-            
+            if key == ord("o"): mode = "overlay"
+            if cv2.getWindowProperty("realsense", cv2.WND_PROP_VISIBLE) < 1: mode = "none"
         
         rate.sleep()
+        #print(time.time()-t)
     
 if __name__ == "__main__":
     try:
-        rs_depth()
+        realsense_depth()
     except rospy.ROSInterruptException:
         pass
