a6cb2fd6f58ed865db47a7641b962f7c6300f48a
rafaelfgs <rafaelfernandesgsilva@hotmail.com>
2020-10-04 10:41:06 -0300

    Change to publish TF with IMU stamp

--- rtabmap_ros/scripts/rtab_broadcaster.py
+++ rtabmap_ros/scripts/rtab_broadcaster.py
@@ -2,46 +2,50 @@
 
 import rospy
 import tf
-from rosgraph_msgs.msg import Clock
+from sensor_msgs.msg import Image
 
 
 tfs = [[(+0.270, +0.000, +0.070), (+0.0, +0.0, +0.0, +1.0), "chassis_init", "rtab_init"],
-       [(-0.270, +0.000, -0.070), (+0.0, +0.0, +0.0, +1.0), "rtab_odom",    "rtab_chassis"],
+       [(-0.270, +0.000, -0.070), (+0.0, +0.0, +0.0, +1.0), "rtab_pose",    "rtab_chassis"],
        [(+0.000, +0.000, +0.090), (+0.0, +0.0, +0.0, +1.0), "rtab_chassis", "rtab_imu"],
-       [(+0.000, +0.000, +0.000), (+0.0, +0.0, +0.0, +1.0), "rtab_odom",    "rtab_d435i"],
+       [(+0.000, +0.000, +0.000), (+0.0, +0.0, +0.0, +1.0), "rtab_pose",    "rtab_d435i"],
        [(+0.000, +0.000, +0.000), (-0.5, +0.5, -0.5, +0.5), "rtab_d435i",   "rtab_rgb"],
        [(+0.000, +0.000, +0.000), (-0.5, +0.5, -0.5, +0.5), "rtab_d435i",   "rtab_depth"]]
 
-clock_pub = False
+freq = 10
+t = 0.0
+
+
+def callback(data):
+    global t
+    t = data.header.stamp.to_sec()
 
 
 def tf_broadcaster():
     
     rospy.init_node("rtab_broadcaster_node", anonymous=True)
-    rospy.Subscriber("/clock", Clock, callback)
+    rospy.Subscriber("/rtabmap/rgb/image_raw", Image, callback)
     
-    t = rospy.Time.now().to_sec()
-    while not clock_pub or (t-rospy.Time.now().to_sec()) < 2.0:
-        rospy.sleep(0.01)
-    
-    if clock_pub:
-        rospy.loginfo("Clock found! Publishing RTAB-Map TFs according to Clock time...")
-    else:
-        rospy.loginfo("Clock not found! Publishing RTAB-Map TFs according to ROS time...")
-    
-    rate = rospy.Rate(1000)
-    while not rospy.is_shutdown():
+    rate = rospy.Rate(freq)
+    while t == 0.0 and not rospy.is_shutdown():
         rate.sleep()
-
-
-def callback(data):
     
-    global clock_pub
-    clock_pub = True
+    rospy.loginfo("Color image subscribed! Publishing RTAB-Map TFs according to RGB Image stamp...")
     
-    for k in range(len(tfs)):
-        msg = tf.TransformBroadcaster()
-        msg.sendTransform(tfs[k][0], tfs[k][1], data.clock, tfs[k][3], tfs[k][2])
+    t_pub = (1.0/freq - 0.001)
+    t_last = 0.0
+    
+    while not rospy.is_shutdown():
+        
+        if (t - t_last) > t_pub:
+            
+            for k in range(len(tfs)):
+                msg = tf.TransformBroadcaster()
+                msg.sendTransform(tfs[k][0], tfs[k][1], rospy.Time.from_sec(t), tfs[k][3], tfs[k][2])
+            
+            t_last = t
+        
+        rate.sleep()        
 
 
 if __name__ == "__main__":
